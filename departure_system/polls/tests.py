41ab60f39c8880a7ad9ffbed2037a00e23b6c050
NikiDeyanov86 <33031929+NikiDeyanov86@users.noreply.github.com>
2022-02-27 18:27:24 +0200

    Course Project

--- departure_system/polls/tests.py
+++ departure_system/polls/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from unittest.mock import Mock, patch
+import json
 
-# Create your tests here.
+from polls.views import find_avg_temperature, find_max_temperature, find_min_temperature
+
+class TestC(TestCase):
+    @patch("requests.post")
+    def test_min_temp(self, mocked_requests):
+        data = json.load(open('./polls/test_temp.json'))
+        mocked_requests.return_value.json = Mock(return_value = data)
+        response = find_min_temperature()
+        assert response == -3
+       
+    @patch("requests.post") 
+    def test_max_temp(self, mocked_requests):
+        data = json.load(open('./polls/test_temp.json'))
+        mocked_requests.return_value.json = Mock(return_value = data)
+        response = find_max_temperature()
+        assert response == 10
+        
+    @patch("requests.post")
+    def test_avg_temp(self, mocked_requests):
+        data = json.load(open('./polls/test_temp.json'))
+        mocked_requests.return_value.json = Mock(return_value = data)
+        response = find_avg_temperature()
+        assert response == 3.2
--- departure_system/polls/views.py
+++ departure_system/polls/views.py
@@ -1,54 +1,49 @@
 from django.shortcuts import render
-
-# Create your views here.
-
+import requests
+import urllib, json
 from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html')
 
-import urllib, json
-
-url = "https://tues2022.proxy.beeceptor.com/my/api/test"
+url = 'https://tues2022.proxy.beeceptor.com/my/api/test'
 
 def return_temperatures(request):
     min = find_min_temperature()
     max = find_max_temperature()
     avg = find_avg_temperature()
     
-    return render(request, 'temperature.html', {'temperatures' : [min, max, avg]})
+    return render(request, 'temperature.html', {'temperatures' : {
+                                                'min' : min,
+                                                'max' : max,
+                                                'avg' : avg
+    } } )
 
 def find_min_temperature():
-    response = urllib.request.urlopen(url)
-    data = json.loads(response.read())
-    print(type(data))
-    min = data["data"]["temperature"]
+    data = requests.post(url).json()
+    min = int(data['data'][0]['temperature'])
     
-    for i in data["data"]:
-        if i["temperature"] < min:
-            min = i["temperature"]
-  
-    return min
+    for i in range(len(data['data'])):
+        if int(data['data'][i]['temperature']) < min:
+            min = int(data['data'][i]['temperature'])
+            
+    return int(min)
 
 def find_max_temperature():
-    response = urllib.request.urlopen(url)
-    data = json.loads(response.read())
-    max = data["data"]["temperature"]
+    data = requests.post(url).json()
+    max = int(data['data'][0]['temperature'])
     
-    for i in data['data']:
-        if i["temperature"] > max:
-            max = i["temperature"]
+    for i in range(len(data['data'])):
+        if int(data['data'][i]['temperature']) > max:
+            max = int(data['data'][i]['temperature'])
   
     return max
 
 def find_avg_temperature():
-    response = urllib.request.urlopen(url)
-    data = json.loads(response.read())
+    data = requests.post(url).json()
     avg = 0
-    br = 0
     
-    for i in data["data"]:
-        avg += i["temperature"]
-        br += 1
+    for i in range(len(data['data'])):
+        avg += float(data['data'][i]['temperature'])
   
-    return avg/br
+    return avg/len(data['data'])
